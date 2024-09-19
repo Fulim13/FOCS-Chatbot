fb909193d17a1a4e30755b795c398e775d0e8067
Fu Lim <programmingwong@gmail.com>
2024-09-19 18:33:14 +0800

    update for pinecone

--- api/backend/grab_data.py
+++ api/backend/grab_data.py
@@ -1,24 +1,23 @@
+import time
+from pinecone import Pinecone, ServerlessSpec
 from langchain_community.document_loaders import TextLoader
-from langchain_mistralai import ChatMistralAI
+from langchain_mistralai import ChatMistralAI, MistralAIEmbeddings
 from pydantic import BaseModel, Field
 from langchain_core.output_parsers import JsonOutputParser
 from langchain_core.pydantic_v1 import BaseModel, Field
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.utils.function_calling import convert_to_openai_tool
 from typing import Optional, List
+from langchain_pinecone import PineconeVectorStore
 import json
 import os
+from uuid import uuid4
+from dotenv import load_dotenv
+load_dotenv()
 
-model = ChatMistralAI(model="mistral-small-latest")
 
-
-class Requirement(BaseModel):
-    exam_qualification: str = Field(
-        description="The exam or qualification required.")
-    grade: str = Field(
-        description="The grade or score required in the exam or qualification.")
-    subject: Optional[str] = Field(
-        description="The subject or area of study required for the exam or qualification.")
+model = ChatMistralAI(model="mistral-small-latest",
+                      mistral_api_key=os.environ["MISTRAL_API_KEY"])
 
 
 class Programme(BaseModel):
@@ -38,10 +37,12 @@ class Programme(BaseModel):
         description="The career perspectives of the programme.")
     estimated_fees: int = Field(
         description="The estimated fees for the programme.")
-    minimum_entry_requirements: List[Requirement] = Field(
+    minimum_entry_requirements: List[str] = Field(
         description="The minimum entry requirements for the programme.")
     academic_progression: Optional[str] = Field(
         description="The academic progression opportunities after completing the programme.")
+    url: str = Field(
+        description="The URL to the programme page.")
 
 
 prompt = ChatPromptTemplate.from_messages([
@@ -62,10 +63,8 @@ file_paths = [
     "./data/Foundation_in_Computing.txt",
     "./data/Diploma_in_Computer_Science.txt",
     "./data/Diploma_in_Information_Technology.txt",
-    "./data/Bachelor_of_Software_Engineering.txt",
-    "./data/Bachelor_of_DS.txt"
 ]
-output_file = "./data/extracted_programme.json"
+output_file = "./data/extracted_programmes.json"
 
 if not os.path.exists(output_file):
     # Load each file separately and combine the results
@@ -74,20 +73,48 @@ if not os.path.exists(output_file):
         loader = TextLoader(path)
         docs.extend(loader.load())  # Combine the documents
 
-    for doc in docs:
-        response = extraction_chain.invoke({"input": doc})
-        # Check if the JSON file exists and load its content if it does
-        try:
-            with open(output_file, 'r') as file:
-                existing_data = json.load(file)
-        except FileNotFoundError:
-            existing_data = []
+    pinecone_api_key = os.environ.get("PINECONE_API_KEY")
+    pc = Pinecone(api_key=pinecone_api_key)
 
-        # Append the new data to the existing content
-        existing_data.append(response)
+    index_name = "focs-index2"
 
-        # Write the updated content back to the JSON file
-        with open(output_file, 'w') as file:
-            json.dump(existing_data, file, indent=4)
+    existing_indexes = [index_info["name"] for index_info in pc.list_indexes()]
 
-        print(f"data appended to {output_file}")
+    if index_name not in existing_indexes:
+        pc.create_index(
+            name=index_name,
+            dimension=1024,
+            metric="cosine",
+            spec=ServerlessSpec(cloud="aws", region="us-east-1"),
+        )
+        while not pc.describe_index(index_name).status["ready"]:
+            time.sleep(1)
+
+    index = pc.Index(index_name)
+
+    embeddings = MistralAIEmbeddings(
+        mistral_api_key=os.environ["MISTRAL_API_KEY"])
+
+    vector_store = PineconeVectorStore(index=index, embedding=embeddings)
+
+    uuids = [str(uuid4()) for _ in range(len(docs))]
+
+    vector_store.add_documents(documents=docs, ids=uuids)
+
+# for doc in docs:
+#     response = extraction_chain.invoke({"input": doc})
+#     # Check if the JSON file exists and load its content if it does
+#     try:
+#         with open(output_file, 'r') as file:
+#             existing_data = json.load(file)
+#     except FileNotFoundError:
+#         existing_data = []
+
+#     # Append the new data to the existing content
+#     existing_data.append(response)
+
+#     # Write the updated content back to the JSON file
+#     with open(output_file, 'w') as file:
+#         json.dump(existing_data, file, indent=4)
+
+#     print(f"data appended to {output_file}")
--- api/backend/views.py
+++ api/backend/views.py
@@ -3,16 +3,17 @@ from rest_framework.response import Response
 import os
 import json
 from langchain.memory import ConversationSummaryMemory
-from langchain_mistralai import ChatMistralAI
+from langchain_mistralai import ChatMistralAI, MistralAIEmbeddings
 from langchain_core.prompts import ChatPromptTemplate
 from pathlib import Path
 from django.conf import settings
 from dotenv import load_dotenv
 import time
-
+from pinecone import Pinecone, ServerlessSpec
+from langchain_pinecone import PineconeVectorStore
 load_dotenv()
-memory = ConversationSummaryMemory(llm=ChatMistralAI(
-    temperature=0, model="mistral-large-latest"))
+# memory = ConversationSummaryMemory(llm=ChatMistralAI(
+#     temperature=0, model="mistral-large-latest"))
 
 MAX_RETRIES = 3  # Define the maximum number of retries
 RETRY_DELAY = 2  # Delay between retries in seconds
@@ -25,46 +26,52 @@ def send_some_data(request):
     output_file = Path(settings.BASE_DIR) / "backend" / \
         "data" / "extracted_programme.json"
 
-    if os.path.exists(output_file):
-        model = ChatMistralAI(model="mistral-small-latest",
-                              mistral_api_key=os.environ["MISTRAL_API_KEY"])
-        question = request.data.get('message', '')
+    model = ChatMistralAI(model="open-mixtral-8x22b",
+                          mistral_api_key=os.environ["MISTRAL_API_KEY"])
+    question = request.data.get('message', '')
 
-        data = json.loads(Path(output_file).read_text())
-        previous_context = str(memory.load_memory_variables({}))
+    # data = json.loads(Path(output_file).read_text())
+    # previous_context = str(memory.load_memory_variables({}))
 
-        prompt = ChatPromptTemplate.from_messages([
-            ("system", "You are an assistant for TARUMT to help students with FOCS university courses. "
-             "This is the history of chat: {history_chat}. "
-             "Answer the question based on the following context:"),
-            ("human", "Question: {question}\n Context: {context}")
-        ])
-        chain = prompt | model
+    index_name = "focs-index2"
+    pinecone_api_key = os.environ.get("PINECONE_API_KEY")
+    pc = Pinecone(api_key=pinecone_api_key)
+    index = pc.Index(index_name)
 
-        retries = 0
-        while retries < MAX_RETRIES:
-            try:
-                result = chain.invoke(
-                    {"question": question, "context": data,
-                        "history_chat": previous_context}
-                )
+    embeddings = MistralAIEmbeddings(
+        mistral_api_key=os.environ["MISTRAL_API_KEY"])
+    vector_store = PineconeVectorStore(index=index, embedding=embeddings)
+    data = vector_store.similarity_search(
+        question,
+        k=1,
+    )
 
-                memory.save_context({"input": question}, {
-                                    "output": result.content})
+    prompt = ChatPromptTemplate.from_messages([
+        ("system", "You are an assistant for TARUMT to help students with FOCS university courses. if possible arrange the answer in numbered/ordered list and at the end of the answer, provide the url of the programmed as source: <a href='url'>Programme Name</a>"
+            "Answer the question based on the following context:"),
+        ("human", "Question: {question}\n Context: {context}")
+    ])
+    chain = prompt | model
 
+    retries = 0
+    while retries < MAX_RETRIES:
+        try:
+            result = chain.invoke(
+                {"question": question, "context": data}
+            )
+
+            # memory.save_context({"input": question}, {
+            #                     "output": result.content})
+
+            return Response({
+                "data": result.content
+            })
+        except Exception as e:
+            retries += 1
+            if retries >= MAX_RETRIES:
                 return Response({
-                    "data": result.content
+                    "error": "Failed to process the request after multiple attempts.",
+                    "details": str(e)
                 })
-            except Exception as e:
-                retries += 1
-                if retries >= MAX_RETRIES:
-                    return Response({
-                        "error": "Failed to process the request after multiple attempts.",
-                        "details": str(e)
-                    })
-                else:
-                    time.sleep(RETRY_DELAY)  # Wait before retrying
-    else:
-        return Response({
-            "data": "No data available"
-        })
+            else:
+                time.sleep(RETRY_DELAY)  # Wait before retrying
